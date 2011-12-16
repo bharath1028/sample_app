541a7f15e1c2c8fe9094ad6ba7b42d7da61f749c
Bharath Sitaraman <bharath1028@gmail.com>
2011-12-16 07:19:42 -0800

    Finished first half of User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   #Returns the base title on a per-page basis
   def title
     base_title = "Ruby on Rails Sample App"
